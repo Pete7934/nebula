9a9cb85f69a92eb9c895b2ea69c4d93d7e1726fc
Peter Karlbye Winther <pete7934@sde.local>
2017-03-24 10:59:21 +0100

    Friday push

--- js/udforsk.js
+++ js/udforsk.js
@@ -6,4 +6,26 @@ for (var i = 0; i < box.length; i++){
         console.log(e);
     });
 }
+
+
+branch = document.getElementsByClassName('udforsk');
+    for(var l = 0; l < branch.length; l++){
+        console.log(branch[l]);
+        branch[l].addEventListener('click', function(){
+            var temp = this.getAttribute('value');
+            clearDisplay(temp);
+        });
+    }
+}
+function clearDisplay(temp){
+    var content = document.getElementsByClassName("content");
+    for(var i = 0; i < content.length; i++){
+        if(content[i].getAttribute("id")==temp){
+            content[i].style.display = "block";
+            
+        }
+        else{
+            content[i].style.display = "none";
+        }
+    }
 }
